e4e1e1110eb39c427d3ac47cf8c997228fa0da22
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2010-10-25 02:05:44 +0900

    fixed rendering bug. added map window.

--- lib/screen.rb
+++ lib/screen.rb
@@ -9,10 +9,12 @@ class MainArea < Window
   def initialize
     super(19, 53, 0, 0)
     @quasi3d = Quasi3d.new(@window)
+    @map = TextWindow.new(15, 28, 25, 0)
 
     @pos = [0,0]
     @eye = [1,0]
-    @maze = Automaze::Automaze.new(:algorithm => :dug_tunnels)
+    @maze = Automaze::Automaze.new(:algorithm => :dug_tunnels, :size_x => 10, :size_y => 10)
+    @map.text = @maze.to_s.delete("\n")
 
     [
       [-4, 3], [ 4, 3], [-2, 3], [ 2, 3], [ 0, 3], [-3, 2], [ 3, 2], [-1, 2],
@@ -23,16 +25,17 @@ class MainArea < Window
   end
 
   def relative_pos(x, y)
-    return [@pos[0] + x, @pos[1] + y] if @eye == [0, 1]
-    return [@pos[0] + x, @pos[1] - y] if @eye == [0, -1]
-    return [@pos[0] + x, @pos[1] + y] if @eye == [1, 0]
-    return [@pos[0] - x, @pos[1] + y] if @eye == [-1, 0]
+    return [@pos[0] + x, @pos[1] + y] if @eye == [0, -1]
+    return [@pos[0] + x, @pos[1] - y] if @eye == [0, 1]
+    return [@pos[0] + x, @pos[1] + y] if @eye == [-1, 0]
+    return [@pos[0] - x, @pos[1] + y] if @eye == [1, 0]
     raise "@eye is invalid"
   end
 
   def refresh
     super
     @quasi3d.refresh
+    @map.refresh
   end
 
   def getch
