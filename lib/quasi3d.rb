7be172daae7c6dc97cf6286c3c058c0cb1d7f604
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2010-10-25 01:53:53 +0900

    trial rendering with 3D maze.

--- lib/quasi3d.rb
+++ lib/quasi3d.rb
@@ -24,20 +24,22 @@ class Quasi3d
       [ 0, 3],
       [-3, 2],
       [ 3, 2],
-      #[-1, 2],
+      [-1, 2],
       [ 1, 2],
       [-4, 1],
       [ 4, 1],
-      #[-2, 1],
+      [-2, 1],
       [ 2, 1],
-      #[ 0, 1],
-      #[-1, 0],
-      #[ 1, 0],
+      [ 0, 1],
+      [-1, 0],
+      [ 1, 0],
     ].each do |xy|
       @panels[xy] = false
-      @panels[xy] = true
     end
+  end
 
+  def set_panel(x, y, bool)
+    @panels[[x,y]] = bool
   end
 
   def refresh
--- lib/screen.rb
+++ lib/screen.rb
@@ -1,10 +1,33 @@
+$: << "../automazerb/lib/"
+require "automaze"
+
 require_relative "text_window"
 require_relative "quasi3d"
+require_relative "debug"
 
 class MainArea < Window
   def initialize
     super(19, 53, 0, 0)
     @quasi3d = Quasi3d.new(@window)
+
+    @pos = [0,0]
+    @eye = [1,0]
+    @maze = Automaze::Automaze.new(:algorithm => :dug_tunnels)
+
+    [
+      [-4, 3], [ 4, 3], [-2, 3], [ 2, 3], [ 0, 3], [-3, 2], [ 3, 2], [-1, 2],
+      [ 1, 2], [-4, 1], [ 4, 1], [-2, 1], [ 2, 1], [ 0, 1], [-1, 0], [ 1, 0],
+    ].each do |xy|
+      @quasi3d.set_panel(*xy, @maze.panels(*relative_pos(*xy)).wall?)
+    end
+  end
+
+  def relative_pos(x, y)
+    return [@pos[0] + x, @pos[1] + y] if @eye == [0, 1]
+    return [@pos[0] + x, @pos[1] - y] if @eye == [0, -1]
+    return [@pos[0] + x, @pos[1] + y] if @eye == [1, 0]
+    return [@pos[0] - x, @pos[1] + y] if @eye == [-1, 0]
+    raise "@eye is invalid"
   end
 
   def refresh
