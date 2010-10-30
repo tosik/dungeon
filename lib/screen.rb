097282f114d94bbf799526efac1f7f52190f2cb5
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2010-10-31 01:06:28 +0900

    added wall([3,0],[-3,0]) to 3D view

--- lib/screen.rb
+++ lib/screen.rb
@@ -39,6 +39,7 @@ class MainArea < Window
     [
       [-4, 3], [ 4, 3], [-2, 3], [ 2, 3], [ 0, 3], [-3, 2], [ 3, 2], [-1, 2],
       [ 1, 2], [-4, 1], [ 4, 1], [-2, 1], [ 2, 1], [ 0, 1], [-1, 0], [ 1, 0],
+      [ 3, 0], [-3, 0],
     ].each do |xy|
       @quasi3d.set_panel(*xy, @maze.panels(*@player.relative_pos(*xy)).wall?)
     end
@@ -49,10 +50,6 @@ class MainArea < Window
     update
     @quasi3d.refresh
     @map.refresh
-  end
-
-  def getch
-    @window.getch
   end
 end
 
