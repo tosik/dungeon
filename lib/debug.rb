18294dc5bdd170491f3e49998d5db90050613336
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2010-10-30 23:36:40 +0900

    cleaning debug console when console is full

--- lib/debug.rb
+++ lib/debug.rb
@@ -1,9 +1,10 @@
 require_relative "text_window"
 
 def d(*args)
-  @win ||= TextWindow.new(25, 31, 0, 80, :scroll=>true)
+  @win ||= TextWindow.new(25, 31, 0, 80)
   @win.text = "------- debug window -------\n" if @win.text.empty?
   @win.text += args.join(" ") + "\n"
+  @win.text = "" if @win.text.count("\n") > 25
   @win.refresh
 end
 
