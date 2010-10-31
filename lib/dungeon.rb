7a7768ba812f799c874653cb6a025a0460c71e86
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2010-10-31 15:40:57 +0900

    removed text from message window.

--- lib/dungeon.rb
+++ lib/dungeon.rb
@@ -11,7 +11,6 @@ class Dungeon
     menu_area.refresh
 
     message_area = MessageArea.new
-    message_area.text = "Here comes Santa Claus! Here comes Santa Claus! Right down Santa Claus Lane! Vixen and Blitzen and all his reindeer are pulling on the reins. Bells are ringing children singing; All is merry and bright. Hang your stockings and say your prayers, 'Cause Santa Claus comes tonight."
     message_area.refresh
 
     loop do
